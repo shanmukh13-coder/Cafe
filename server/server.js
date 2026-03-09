3e517f6b3d10661edc39298092fa86ec25a79ba8
shanmukh13-coder <pshanukh13@gmail.com>
2026-03-09 16:05:42 +0530

    Setup Node.js Express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Cafe API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
